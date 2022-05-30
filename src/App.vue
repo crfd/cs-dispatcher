19c36135950f3b48ffed92c4c36291d1736921a9
André Kuhlmann <akuhltime@gmail.com>
2022-05-30 23:58:32 +0200

    Set html background white/1

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,19 @@
   box-sizing: border-box;
 }
 
+html,
+body {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  @apply bg-white/1;
+}
+
 #app {
   font-family: Inter, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   font-size: 15px;
 }
 </style>
