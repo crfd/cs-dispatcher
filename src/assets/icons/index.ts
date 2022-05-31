cafd83f09d7d204999a8aaea92701bb916598244
André Kuhlmann <akuhltime@gmail.com>
2022-05-31 11:56:51 +0200

    Add ArrowCircleRight export

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,6 @@ import InputSuccess from './input-success.svg'
 import InputAutofill from './input-autofill.svg'
 import Plus from './plus.svg'
 import Done from './done.svg'
+import ArrowCircleRight from './arrow-circle-right.svg'
 
-export { InputError, InputSuccess, InputAutofill, Plus, Done }
+export { InputError, InputSuccess, InputAutofill, Plus, Done, ArrowCircleRight }
