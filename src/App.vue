ffbf80b96a9ae40f166a3455e35f935bff991c64
André Kuhlmann <akuhltime@gmail.com>
2022-06-01 16:16:55 +0200

    Add Header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="navbar">
-    <crfd-navbar />
+  <header>
+    <h1 class="title">
+      <img src="./assets/logo-leaf.svg" width="28" class="inline-block" />
+      Confined Space Dispatcher
+    </h1>
+    <div class="navbar">
+      <crfd-navbar />
+    </div>
+  </header>
+  <div id="content" ref="content">
+    <router-view></router-view>
   </div>
-  <router-view></router-view>
 </template>
 
 <script>
@@ -11,6 +19,16 @@ import CRFDNavbar from './components/CRFDNavbar.vue'
 export default {
   components: {
     'crfd-navbar': CRFDNavbar
+  },
+  data() {
+    return {
+      isScrolled: false
+    }
+  },
+  mounted() {
+    this.$refs.content.addEventListener('scroll', () => {
+      this.isScrolled = this.$refs.content.scrollTop > 0
+    })
   }
 }
 </script>
@@ -25,18 +43,35 @@ body {
   position: relative;
   height: 100%;
   width: 100%;
-  @apply bg-white/primary;
+  overflow: hidden;
+  @apply bg-white/1;
 }
 
-#app {
-  font-family: Inter, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  font-size: 15px;
+#content {
+  box-sizing: border-box;
+  position: absolute;
+  width: 100vw;
+  height: calc(100vh - 88px);
+  overflow-y: scroll;
+  z-index: 0;
+}
+
+header {
+  position: relative;
+  padding-bottom: -2px;
+  @apply bg-white/primary transition-all ease-in-out;
+}
+
+.scroll {
+  overflow-y: scroll;
+}
+
+.title {
+  @apply mx-3 flex items-center gap-2 py-3 text-left text-lg font-semibold;
 }
 
 .navbar {
+  position: relative;
   @apply border-b-2 border-solid border-white/2;
 }
 </style>
