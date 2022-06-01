b6ffbcbf7073188227cb62a3c590c451919144d1
André Kuhlmann <akuhltime@gmail.com>
2022-06-01 15:34:42 +0200

    Add icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,6 +3,23 @@ import InputSuccess from './input-success.svg'
 import InputAutofill from './input-autofill.svg'
 import Plus from './plus.svg'
 import Done from './done.svg'
+import Home from './home.svg'
+import Gear from './gear.svg'
+import PieChart from './pie-chart.svg'
+import City from './city.svg'
+import Runner from './runner.svg'
 import ArrowCircleRight from './arrow-circle-right.svg'
 
-export { InputError, InputSuccess, InputAutofill, Plus, Done, ArrowCircleRight }
+export {
+  InputError,
+  InputSuccess,
+  InputAutofill,
+  Plus,
+  Done,
+  ArrowCircleRight,
+  Home,
+  Gear,
+  PieChart,
+  City,
+  Runner
+}
