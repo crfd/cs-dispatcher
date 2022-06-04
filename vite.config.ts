54f8c3516f4733d3b23f4c72e83f570755e252e6
André Kuhlmann <akuhltime@gmail.com>
2022-06-04 17:54:27 +0200

    Install rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
 // Path configuration see:
 // https://divotion.com/blog/how-to-configure-import-aliases-in-vite-typescript-and-jest
 
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
