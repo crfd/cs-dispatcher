1ee3816ef49f0cff825509302a2d1e127ff19c17
André Kuhlmann <akuhltime@gmail.com>
2022-06-04 18:03:50 +0200

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
-  trailingComma: "none",
+  trailingComma: 'none',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  arrowParens: 'avoid'
 };
