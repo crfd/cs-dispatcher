c0812496c0707113551fb5a74f79aecd1b04e013
André Kuhlmann <akuhltime@gmail.com>
2022-06-05 21:22:31 +0200

    Remove index files
    As components get auto exported / imported the index files are no longer needed

--- src/layouts/index.ts
+++
@@ -1,5 +0,0 @@
-import InPageNavigationLayout from './InPageNavigationLayout.vue'
-import RightDetailLayout from './RightDetailLayout.vue'
-import TableLayout from './TableLayout.vue'
-
-export { InPageNavigationLayout, RightDetailLayout, TableLayout }
