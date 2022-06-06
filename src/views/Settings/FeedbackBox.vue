7a1aa388e97b8632234f74c8e8815e68bf3bb537
André Kuhlmann <akuhltime@gmail.com>
2022-06-06 11:58:20 +0200

    Setup file selection for feedback view

--- src/views/Settings/FeedbackBox.vue
+++ src/views/Settings/FeedbackBox.vue
@@ -7,10 +7,21 @@
     <CRFDInput v-model="name" placeholder="Name" />
     <CRFDTextarea v-model="feedback" />
     <HFlex>
-      <CRFDButton flavour="secondary">Select File</CRFDButton>
-      <span class="text-black/2">No File selected</span>
+      <CRFDButton @click="fileSelectionHandler" flavour="secondary">
+        Select File
+      </CRFDButton>
+      <span class="text-black/2">{{ fileText }}</span>
+      <div class="clear-icon-btn" v-if="file">
+        <i-crfd-cancle @click="clearFile" width="24px" />
+      </div>
       <div class="spacer"></div>
-      <p>Max File Size: 5MB</p>
+      <div>
+        <p v-if="file && exceedsMaxFileSize" class="text-red/primary">
+          The file is too large.
+        </p>
+        <p v-else-if="file" class="text-green/primary">File selected</p>
+        <p v-else>Max File Size: 5MB</p>
+      </div>
     </HFlex>
     <template #footer-left>
       <span v-if="invalid">There are still some fields missing</span>
@@ -24,18 +35,30 @@
 </template>
 
 <script>
+import { fileDialog } from 'file-select-dialog'
+
 export default {
   name: 'FeedbackBox',
   data() {
     return {
       name: '',
       feedback: '',
-      file: null
+      file: null,
+      maxFileSizeMB: 5
     }
   },
   computed: {
     invalid() {
-      return this.name === '' || this.feedback === ''
+      return this.name === '' || this.feedback === '' || this.exceedsMaxFileSize
+    },
+    fileSize() {
+      return this.file?.size ?? 0
+    },
+    exceedsMaxFileSize() {
+      return this.fileSize > this.maxFileSizeMB * 1024 * 1024
+    },
+    fileText() {
+      return this.file ? this.file.name : 'No File selected'
     }
   },
   methods: {
@@ -44,8 +67,17 @@ export default {
       this.feedback = ''
       this.file = null
     },
+    clearFile() {
+      this.file = null
+    },
     send() {
       console.log('send')
+    },
+    fileSelectionHandler() {
+      fileDialog().then(files => {
+        this.file = files[0]
+        console.dir(files[0])
+      })
     }
   }
 }
@@ -55,6 +87,9 @@ export default {
 .box {
   display: flex;
   flex-direction: column;
-  gap: 12px;
+}
+
+.clear-icon-btn {
+  @apply cursor-pointer text-black/2;
 }
 </style>
