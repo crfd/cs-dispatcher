eb36cc3bbf202a9caa3a90af9755e2331efe1c71
André Kuhlmann <akuhltime@gmail.com>
2022-06-15 10:34:11 +0200

    Update LoremIpsum.vue

--- src/components/LoremIpsum.vue
+++ src/components/LoremIpsum.vue
@@ -1,36 +1,49 @@
+<script lang="ts" setup>
+import { useFetch } from '@vueuse/core'
+import { computed, ComputedRef, onMounted, Ref, ref, toRefs } from 'vue'
+
+// =====
+// PROPS
+// =====
+
+const props = defineProps({
+  paragraphs: [Number, String]
+})
+
+const { paragraphs } = toRefs(props)
+
+// ====
+// DATA
+// ====
+
+const text = ref('Loading ...')
+
+// ========
+// COMPUTED
+// ========
+
+const url = computed(() => {
+  return `https://baconipsum.com/api/?type=meat-and-filler&paras=${
+    paragraphs?.value ?? 10
+  }&format=text`
+})
+
+// =========
+// LIFECYCLE
+// =========
+
+onMounted(async () => {
+  try {
+    const res = await fetch(url.value)
+    text.value = await res.text()
+  } catch (e) {
+    text.value = (e as Error).message
+  }
+})
+</script>
+
 <template>
   <p>
     {{ text }}
   </p>
 </template>
-
-<script>
-export default {
-  name: 'lorem-ipsum',
-  props: {
-    paragraphs: {
-      type: [String, Number],
-      default: 20
-    }
-  },
-  data() {
-    return {
-      text: 'Loading ...'
-    }
-  },
-  computed: {
-    url() {
-      return `https://baconipsum.com/api/?type=meat-and-filler&paras=${this.paragraphs}&format=text`
-    }
-  },
-  created() {
-    fetch(this.url)
-      .then(response => response.text())
-      .then(text => (this.text = text))
-      .catch(error => {
-        console.error(error)
-        this.text = 'Error: ' + error.message
-      })
-  }
-}
-</script>
